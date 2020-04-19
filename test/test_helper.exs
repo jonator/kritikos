42f9a1f420bf7787d3cd947a39bcb20f6ad555a9
Jon Ator <jonathanator0@gmail.com>
2020-04-19 14:10:53 -0500

    Add dashboard controller tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,17 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Kritikos.Repo, :manual)
+
+defmodule Helpers do
+  def put_user_auth_cookie(conn) do
+    {:ok, user} =
+      Kritikos.Auth.register_user(%{
+        email: "mike@email.com",
+        first_last_name: "mike a",
+        password: "a",
+        password_confirmation: "a"
+      })
+
+    conn
+    |> Plug.Test.init_test_session(%{user: user})
+  end
+end
