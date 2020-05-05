4f0250276419ae6b589c2b0df93327dc5ad3cb37
Jon Ator <jonathanator0@gmail.com>
2020-05-05 10:56:55 -0500

    Add support info

--- assets/js/dashboard/components/subviews/Settings.vue
+++ assets/js/dashboard/components/subviews/Settings.vue
@@ -28,11 +28,71 @@
         </tr>
       </table>
     </div>
+    <div id="support">
+      Support
+      <table>
+        <col width="40%" />
+        <tr>
+          <td id="desc">
+            New email
+            <i class="gg-external"></i>
+          </td>
+          <td>
+            <a href="mailto:support@kritikos.app">support@kritikos.app</a>
+          </td>
+        </tr>
+        <tr>
+          <td id="desc">Feedback</td>
+          <td>
+            <a href="https://kritikos.app/kritikos" target="_blank">kritikos.app/kritikos</a>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <style scoped>
 #change-password-button {
   margin: 0;
+}
+#support #desc {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+/* external icon */
+.gg-external {
+  box-sizing: border-box;
+  position: relative;
+  display: block;
+  transform: scale(var(--ggs, 1));
+  width: 12px;
+  height: 12px;
+  box-shadow: -2px 2px 0 0, -4px -4px 0 -2px, 4px 4px 0 -2px;
+  margin-left: -2px;
+  margin-top: 1px;
+}
+.gg-external::after,
+.gg-external::before {
+  content: "";
+  display: block;
+  box-sizing: border-box;
+  position: absolute;
+  right: -4px;
+}
+.gg-external::before {
+  background: currentColor;
+  transform: rotate(-45deg);
+  width: 12px;
+  height: 2px;
+  top: 1px;
+}
+.gg-external::after {
+  width: 8px;
+  height: 8px;
+  border-right: 2px solid;
+  border-top: 2px solid;
+  top: -4px;
 }
 </style>
