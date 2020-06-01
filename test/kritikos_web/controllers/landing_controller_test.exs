65ec933c31b51b1700126e8ca027e4c3e7430347
Jon Ator <jonathanator0@gmail.com>
2020-06-01 11:39:06 -0500

    Add portal controller test, fix tests

--- test/kritikos_web/controllers/landing_controller_test.exs
+++ test/kritikos_web/controllers/landing_controller_test.exs
@@ -9,14 +9,4 @@ defmodule KritikosWeb.LandingControllerTest do
     assert html_response(conn, 200) =~
              "<li>patients</li>\n                <li>users</li>\n                <li>clients</li>\n                <li>attendees</li>\n                <li>audience members</li>"
   end
-
-  test "open portal", %{conn: conn} do
-    conn = get(conn, "/portal")
-
-    assert html_response(conn, 200) =~
-             "<h1 class=\"clickable\" onclick=\"window.location.href='/'\">Kritikos</h1>"
-
-    assert html_response(conn, 200) =~
-             "<script type=\"text/javascript\" src=\"/js/landing.js\"></script>"
-  end
 end
